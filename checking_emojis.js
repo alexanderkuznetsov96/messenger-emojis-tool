2a5a2e323ae3c21e5b0e0a3b0446d0223358f77b
Alexander Kuznetsov <alexander.kuznetsov96@gmail.com>
2019-06-28 17:52:43 -0700

    Added pseudocode

--- checking_emojis.js
+++ checking_emojis.js
@@ -1,3 +1,8 @@
 var emoji = require('emoji.json')
 console.log(emoji[2])
 console.log(emoji[2]["codes"])
+
+// Psuedocode
+// For each emoji in emojs
+//   check if https://static.xx.fbcdn.net/images/emoji.php/v9/teb/1.5/128/<emoji-code>.png exists (i.e. doesn't 404)
+// list emojis that exist
